95adf304b98e4cd0371f284d78dac16a0c92c007
skysmiter <glo_age@hotmail.com>
2022-02-11 17:19:42 +0100

    Added Tutorial & Menu & permission question

---
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager
+{
+    public enum Scene
+    {
+        //Home, Tutorial, Credits, Game, OpenSettings
+        Home, Tutorial, Game, OpenSettings
+    }
+
+
+    // Public Methods
+
+    public static void LoadScene(Scene scene)
+    {
+        SceneManager.LoadSceneAsync(SceneName(scene));
+    }
+
+    // Helper Methods
+
+    private static string SceneName(Scene scene)
+    {
+        switch (scene)
+        {
+            case Scene.Home: return "Home";
+            case Scene.Game: return "Game";
+            case Scene.Tutorial: return "Tutorial";
+            //case Scene.Credits: return "Credits";
+            case Scene.OpenSettings: return "OpenSettings";
+        }
+        return "";
+    }
+}
